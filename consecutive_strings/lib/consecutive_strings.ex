607ccfe33569c838721f2f9fc687a9c64dc19ae3
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-07-14 22:01:12 +0200

    Passing half the tests

--- consecutive_strings/lib/consecutive_strings.ex
+++ consecutive_strings/lib/consecutive_strings.ex
@@ -1,7 +1,26 @@
 defmodule Longestconsec do
 
   def longest_consec(strarr, k) do
-    # your code
+    consecutives(strarr, k)
+    |> Enum.map(&(Enum.join(&1)))
+    |> Enum.max_by(&(String.length(&1)))
   end
 
+  defp consecutives(strarr, k) when length(strarr) == 0 do
+    [[""]]
+  end
+
+  defp consecutives(strarr, k) do
+    Stream.unfold(length(strarr)-k, fn
+      -1 -> nil
+       n -> {
+        strarr
+        |> Enum.drop(n)
+        |> Enum.take(k),
+        n-1
+      }
+    end)
+    |> Enum.to_list()
+    |> Enum.reverse
+  end
 end
