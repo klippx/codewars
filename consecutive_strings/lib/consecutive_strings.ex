9624075a5734c085d810684fbe6862d0254cef56
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-07-14 22:21:45 +0200

    Refactor

--- consecutive_strings/lib/consecutive_strings.ex
+++ consecutive_strings/lib/consecutive_strings.ex
@@ -1,21 +1,12 @@
 defmodule Longestconsec do
 
+  def longest_consec(strarr, _) when length(strarr) == 0, do: ""
+  def longest_consec(_, k) when k < 1, do: ""
+  def longest_consec(strarr, k) when k > length(strarr), do: ""
   def longest_consec(strarr, k) do
     consecutives(strarr, k)
     |> Enum.map(&(Enum.join(&1)))
     |> Enum.max_by(&(String.length(&1)))
-  end
-
-  defp consecutives(strarr, _) when length(strarr) == 0 do
-    [[""]]
-  end
-
-  defp consecutives(_, k) when k < 1 do
-    [[""]]
-  end
-
-  defp consecutives(strarr, k) when k > length(strarr) do
-    [[""]]
   end
 
   defp consecutives(strarr, k) do
