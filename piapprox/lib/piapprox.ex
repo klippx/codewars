e7c8a377dc3b996352396b48888b890ef7a6081e
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-07-17 13:02:26 +0200

    Its a draft solution, folks. With rounding issues, folks.

--- piapprox/lib/piapprox.ex
+++ piapprox/lib/piapprox.ex
@@ -1,8 +1,8 @@
 defmodule Piapprox do
 
   def iter_pi(epsilon) do
-    Enum.reduce_while(leibniz_stream, 0, fn i, acc ->
-      if abs(:math.pi/4 - acc) > epsilon, do: {:cont, acc + i}, else: {:halt, acc}
+    Enum.reduce_while(leibniz_stream, 0, fn {i, n}, acc ->
+      if abs(:math.pi - 4*acc) > epsilon, do: {:cont, acc + i}, else: {:halt, [n, Float.round(acc*4, 10)]}
     end)
   end
 
@@ -10,6 +10,6 @@ defmodule Piapprox do
     Stream.unfold(1, fn
       n when rem(n,4) == 1 -> { 1/n, n+2 }
       n                    -> {-1/n, n+2 }
-    end)
+    end) |> Stream.with_index
   end
 end
