b5d5d3ab88c6bb7b5766c4a4a711427a159f76f9
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-07-17 12:48:06 +0200

    Its a start, folks

--- piapprox/lib/piapprox.ex
+++ piapprox/lib/piapprox.ex
@@ -1,7 +1,15 @@
 defmodule Piapprox do
 
   def iter_pi(epsilon) do
-    # your code
+    Enum.reduce_while(leibniz_stream, 0, fn i, acc ->
+      if abs(:math.pi/4 - acc) > epsilon, do: {:cont, acc + i}, else: {:halt, acc}
+    end)
   end
 
+  defp leibniz_stream do
+    Stream.unfold(1, fn
+      n when rem(n,4) == 1 -> { 1/n, n+2 }
+      n                    -> {-1/n, n+2 }
+    end)
+  end
 end
