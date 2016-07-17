32e798a3a4dfec4bc6b4447ab396c5394943d291
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-07-17 13:36:07 +0200

    Move 4 to series instead

--- piapprox/lib/piapprox.ex
+++ piapprox/lib/piapprox.ex
@@ -2,14 +2,14 @@ defmodule Piapprox do
 
   def iter_pi(epsilon) do
     Enum.reduce_while(leibniz_stream, 0, fn {i, n}, acc ->
-      if abs(:math.pi - 4*acc) > epsilon, do: {:cont, acc + i}, else: {:halt, [n, Float.round(acc*4, 10)]}
+      if abs(:math.pi - acc) >= epsilon, do: {:cont, acc + i}, else: {:halt, [n, Float.round(acc, 10)]}
     end)
   end
 
   defp leibniz_stream do
     Stream.unfold(1, fn
-      n when rem(n,4) == 1 -> { 1/n, n+2 }
-      n                    -> {-1/n, n+2 }
+      n when rem(n,4) == 1 -> { 4/n, n+2 }
+      n                    -> {-4/n, n+2 }
     end) |> Stream.with_index
   end
 end
