9b4653f2ea7ad8b0de61ce8625e783547a898d51
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-07-20 21:07:42 +0200

    Refactor

--- palindromic_primes/lib/prime.ex
+++ palindromic_primes/lib/prime.ex
@@ -11,16 +11,11 @@ defmodule Prime do
 
   defp next_prime([]), do: 2
   defp next_prime(primes) when is_list primes do
-    primes
-    |> next_primes(1)
-    |> List.first
-  end
-
-  defp next_primes(primes, quantity) when is_list primes and is_integer quantity do
     List.first(primes) + 1
     |> Stream.iterate(&(&1+1))
     |> Stream.drop_while(&(not_palindrome?(&1) || is_divisible?(&1, primes)))
-    |> Enum.take(quantity)
+    |> Enum.take(1)
+    |> List.first
   end
 
   defp is_divisible?(number, primes) when is_list primes and is_integer number do
