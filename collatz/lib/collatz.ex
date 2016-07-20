0f7c561970464d5dd25018baecf0b8b1d32b5a7f
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-07-20 13:32:41 +0200

    Solution

--- collatz/lib/collatz.ex
+++ collatz/lib/collatz.ex
@@ -1,2 +1,13 @@
 defmodule Collatz do
+
+  def collatz(n) do
+    collatz_sequence(n)
+    |> List.flatten
+    |> Enum.join("->")
+  end
+
+  defp collatz_sequence(1), do: [1]
+  defp collatz_sequence(n) when rem(n,2) == 0, do: [n, collatz(div(n,2))]
+  defp collatz_sequence(n), do: [n, collatz(3 * n + 1)]
+
 end
--- collatz/test/collatz_test.exs
+++ collatz/test/collatz_test.exs
@@ -1,8 +1,14 @@
 defmodule CollatzTest do
   use ExUnit.Case
-  doctest Collatz
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  def testCollatz(nb, n, ans) do
+    IO.puts("Test #{nb}")
+    assert Collatz.collatz(n) == ans
+  end
+
+  test "collatz" do
+    testCollatz 1, 2, "2->1"
+    testCollatz 2, 3, "3->10->5->16->8->4->2->1"
+    testCollatz 3, 4, "4->2->1"
   end
 end
