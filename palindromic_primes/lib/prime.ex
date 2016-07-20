88be28f86d8aab09ab23b002323b325569dc7344
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-07-20 19:25:21 +0200

    Simplified Prime

--- palindromic_primes/lib/prime.ex
+++ palindromic_primes/lib/prime.ex
@@ -1,24 +1,15 @@
 defmodule Prime do
 
-  def stream do
+  def take(n) when is_integer(n), do: Enum.take(stream, n)
+
+  defp stream do
     Stream.unfold([], fn primes ->
       next = next_prime(primes)
       { next, [next | primes] }
     end)
   end
 
-  def take(n) when is_integer(n), do: Enum.take(stream, n)
-
-  def get(n) when is_integer(n), do: take(n) |> List.last
-
-  def up_to(n) when is_integer(n) do
-    stream
-    |> Stream.take_while(&(&1 < n))
-    |> Enum.to_list
-  end
-
   defp next_prime([]), do: 2
-
   defp next_prime(primes) when is_list primes do
     primes
     |> next_primes(1)
@@ -26,17 +17,20 @@ defmodule Prime do
   end
 
   defp next_primes(primes, quantity) when is_list primes and is_integer quantity do
-    from = List.first(primes) + 1
-    integers(from)
-    |> Stream.drop_while(fn number ->
-         Enum.any?(primes, fn prime ->
-           rem(number, prime) == 0
-         end)
-       end)
+    List.first(primes) + 1
+    |> Stream.iterate(&(&1+1))
+    |> Stream.drop_while(&(not_palindrome?(&1) || is_divisible?(&1, primes)))
     |> Enum.take(quantity)
   end
 
-  defp integers(n) when is_integer(n) do
-    Stream.iterate(n, &(&1+1))
+  defp is_divisible?(number, primes) when is_list primes and is_integer number do
+    Enum.any?(primes, fn prime ->
+      rem(number, prime) == 0
+    end)
+  end
+
+  defp not_palindrome?(number) when is_integer number do
+    string = Integer.to_string(number)
+    string != String.reverse(string)
   end
 end
