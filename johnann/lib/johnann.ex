c82a5f899356d240a3b4f6982bd2681ee31558e3
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-07-22 20:56:47 +0200

    Found solution for ann/1 and john/1

--- johnann/lib/johnann.ex
+++ johnann/lib/johnann.ex
@@ -1,2 +1,32 @@
 defmodule Johnann do
+
+  def john(n) when n == 0, do: []
+  def john(n) when n >= 1 do
+    john(n-1) ++ [pjohn(n-1)]
+  end
+
+  def ann(n) when n == 0, do: []
+  def ann(n) when n >= 1 do
+    ann(n-1) ++ [pann(n-1)]
+  end
+
+  defp pjohn(n) when n == 0, do: 0
+  defp pjohn(n) when n >= 1 do
+    t = pjohn(n-1)
+    n - pann(t)
+  end
+
+  defp pann(n) when n == 0, do: 1
+  defp pann(n) when n >= 1 do
+    t = pann(n-1)
+    n - pjohn(t)
+  end
+
+  def sum_john(n) when n >= 1 do
+    # your code
+  end
+  def sum_ann(n) when n >= 1 do
+    # your code
+  end
+
 end
--- johnann/test/johnann_test.exs
+++ johnann/test/johnann_test.exs
@@ -1,8 +1,37 @@
 defmodule JohnannTest do
-  use ExUnit.Case
-  doctest Johnann
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  use ExUnit.Case
+
+  defp testJohn(numtest, n, ans) do
+    IO.puts("Johnann.john/1 #{numtest} \n")
+    assert Johnann.john(n) == ans
   end
+  test "john" do
+    testJohn(1, 11, [0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6])
+  end
+
+  defp testAnn(numtest, n, ans) do
+    IO.puts("Johnann.ann/1 #{numtest} \n")
+    assert Johnann.ann(n) == ans
+  end
+  test "ann" do
+    testAnn(1, 6, [1, 1, 2, 2, 3, 3])
+  end
+
+  defp testSumAnn(numtest, n, ans) do
+    IO.puts("Johnann.sum_ann/1 \n")
+    assert Johnann.sum_ann(n) == ans
+  end
+  test "sum_ann" do
+    testSumAnn(1, 115, 4070)
+  end
+
+  defp testSumJohn(numtest, n, ans) do
+    IO.puts("Johnann.sum_john/1 #{numtest} \n")
+    assert Johnann.sum_john(n) == ans
+  end
+  test "sum_john" do
+    testSumJohn(1, 75, 1720)
+  end
+
 end
